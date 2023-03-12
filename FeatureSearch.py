2fc998e5351a51b0949cd60992fd69c83ae7cc89
gkiler <gwenkiler@gmail.com>
2023-03-11 20:59:19 -0800

    a lot of stuff idk. i fixed all the bugs

--- FeatureSearch.py
+++ FeatureSearch.py
@@ -11,9 +11,11 @@ class Validator:
     
     def Accuracy(self):
         accuracy = 0
-        df = self.dataset
+        # self.feature_subset.remove(0)
+        df = self.dataset.iloc[:,1:]
+        # print(df)
         df = df.iloc[:,self.feature_subset]
-        print(df)
+        # print(df)
         for i in range(0,len(df.iloc[:,0])):
             #get slice from 0->index and index+1->end
             n = df.index.isin([i])
@@ -21,13 +23,14 @@ class Validator:
             test = df.iloc[i,:]
             self.classifier.Train(train)
             label_output = self.classifier.Test(test)
-            if test.iloc[0] == label_output:
+            if self.labels[i] == label_output:
                 accuracy += 1
         accuracy = accuracy / (1.0*len(df.iloc[:,0]))
         return accuracy
 
 class Classifier:
-    def __init__(self):
+    def __init__(self,labels):
+        self.labels = labels
         # self.train_instances = []
         pass
 
@@ -37,19 +40,21 @@ class Classifier:
 
     def Test(self,test_instance):
         small = 99999999999
+        # print(test_instance)
+        # print(self.train_instances)
         for instance in self.train_instances.iterrows():
             dist = self.Euclidean_Distance(instance,test_instance)
             if dist < small:
                 small = dist
-                label = instance[1].iloc[0]
+                label = self.labels[instance[0]]
         return label
 
     def Euclidean_Distance(self,one,two):
         #for each x y do (x-y) squared added then sqrt
         sum = 0
-        # print(list(two[1:]))
-        # print(list(one[1][1:]))
-        for (x,y) in zip(list(one[1][1:]),list(two[1:])):
+        # print(list(one[1]))
+        # print(list(two))
+        for (x,y) in zip(list(one[1]),list(two)):
             sum += (x-y)**2 
         return (sum**.5)
 
@@ -65,7 +70,7 @@ class Node:
         self.features.append(feature)
 
     def remove(self,feature):
-        placeholder = [i for i in self.features if i.id != feature.id]
+        placeholder = [i for i in self.features if i != feature]
         self.features = placeholder
     def len(self):
         return len(self.features)
@@ -81,15 +86,17 @@ class Node:
         return f"{self.features}"
 
 def ForwardSelection(features,df):
+    features.remove(0)
     features_len = features.len() # Number of features
     #use feature size to start from 1, select 1 features, then next
     #to select that many features, permutate by
     labels = df.iloc[:,0] #preserve labels
     df = df #ive made a terrible mistake
     greedy_features = Node([],features.dataset)
+    greedy_features.remove(0)
     remaining_features = copy.deepcopy(features)
     curr_best = 0 
-    for i in range(1,features_len+1):
+    for i in range(1,features_len+2):
         #current best is greedy features evaluation
         #i need to remove the 0 that got in there somehow but like keep the labels
         start_len = greedy_features.len()
@@ -102,8 +109,7 @@ def ForwardSelection(features,df):
             #add each one feature at a time
             test_features.append(feature)
             #if this feature set is better than our previous best, then its the new best
-            v = Validator(test_features.features,Classifier(),df)
-            test_features.features
+            v = Validator(test_features.features,Classifier(labels),df)
             acc = test_features.Evaluate(v)
             print(f"Using feature(s) {test_features} accuracy is {acc}")
             if acc > curr_best:
@@ -116,23 +122,30 @@ def ForwardSelection(features,df):
             print(f'Feature set {greedy_features} was best, accuracy is {greedy_features.value}\n')
             remaining_features.remove(new_feature)
         else:
-            print('Accuracy decreased, ending feature search')
+            print('Accuracy decreased or no more nodes to explore, ending feature search')
             #if adding more features is bad then stop
             break
+    print(f'Best feature set was {greedy_features}, accuracy is {greedy_features.value}\n')
     return greedy_features
 
 def BackwardElimination(features,df):
+    print(df)
+    features.remove(0)
+
     features_len = features.len()
     greedy_features = features
-
-    for i in range(1,features_len+1):
-        curr_best = greedy_features.value
+    labels = df.iloc[:,0]
+    for i in range(1,features_len+2):
+        v = Validator(greedy_features.features,Classifier(labels),df)
+        curr_best = greedy_features.Evaluate(v)
         start_len = greedy_features.len()
         loop_features = copy.deepcopy(greedy_features)
         for feature in loop_features.features:
             test_features = copy.deepcopy(loop_features)
             test_features.remove(feature)
-            acc = test_features.value
+
+            v = Validator(test_features.features,Classifier(labels),df)
+            acc = test_features.Evaluate(v)
             print(f"Using feature(s) {test_features} accuracy is {acc}")
             if acc > curr_best:
                 greedy_features = copy.deepcopy(test_features)
@@ -161,8 +174,8 @@ def main(feature_set,dataset):
         BackwardElimination(feature_set,dataset)
 
 def datainput():
-    dataset_path = 'CS170_Spring_2022_Small_data__39.txt' 
-    # dataset_path = 'CS170_Spring_2022_Large_data__39.txt'
+    # dataset_path = 'CS170_Spring_2022_Small_data__39.txt' 
+    dataset_path = 'CS170_Spring_2022_Large_data__39.txt'
     data = []
     with open(dataset_path,'r') as f:
         for line in f:
@@ -184,7 +197,9 @@ def datainput():
     df.iloc[:,1:] = df.iloc[:,1:].apply(znorm)
     # classifier = Classifier()
     feature_set = [] #should be set to 0 when doing self select
+    print("Bear in mind that indices are offset, so input 0 for feature 1, 1 for feature 2 etc")
     num_features_want = int(input('Input desired number of features to select: '))
+    
     for i in range(0,num_features_want):
         x = int(input("\nInput next desired feature subset: "))
         feature_set.append(x)
